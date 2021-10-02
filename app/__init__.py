cfcdd5b3991224d6a2d57b4b96b1fd93197bb99f
Luke Moll <lukemoll@users.noreply.github.com>
2021-10-02 19:04:18 +0100

    Fix that up real quick

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ app = flask.Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
 app.config["FLASK_DEBUG"] = True
 app.config["FLASK_ENV"] = "development"
 app.config["FREEZER_DESTINATION"] = os.path.join(BASE_DIR, "build")
+app.config["FREEZER_DESTINATION_IGNORE"] = ['.git*']
 
 def list_emails() -> Generator[Tuple[date, str], None, None]:
     for filename in os.listdir(EMAIL_DIR):
