9ad4719b59f3dff8fade620acddd418f9ffdd6cb
Luke Moll <lukemoll@users.noreply.github.com>
2021-10-02 18:58:16 +0100

    Fixed some things

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,24 @@
+import flask
+
+from flask_frozen import Freezer
+
 from datetime import date
 from typing import Generator, Tuple, List
-import flask
 import os
-
 from operator import itemgetter
+
+
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 EMAIL_DIR = os.path.join(BASE_DIR, "emails")
 app = flask.Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
 app.config["FLASK_DEBUG"] = True
 app.config["FLASK_ENV"] = "development"
-
+app.config["FREEZER_DESTINATION"] = os.path.join(BASE_DIR, "build")
 
 def list_emails() -> Generator[Tuple[date, str], None, None]:
     for filename in os.listdir(EMAIL_DIR):
-        yield date.fromisoformat(filename[:10]), filename
+        yield date.fromisoformat(filename[:10]), filename.removesuffix(".html")
 
 @app.route("/email/<string:fn>.html")
 def email(fn):
@@ -25,3 +29,9 @@ def index():
     emails = list(list_emails())
     emails.sort(key=itemgetter(0), reverse=True)
     return flask.render_template("index.html.jinja2", emails=emails, foo="bar")
+
+freezer = Freezer(app)
+
+@app.cli.command("build")
+def build():
+    freezer.freeze()
